532d974966f0db8c705c42c0591a37b7bc7a262c
aaronC34 <aaronchun305@gmail.com>
2024-08-02 00:54:15 -0700

    Made Changes to scheduleHandler class

--- ScheduleHandler.java
+++ ScheduleHandler.java
@@ -1,5 +1,33 @@
+import java.util.*;
 public class ScheduleHandler {
+    private CSVHandler csvHandler;
+    private HashMap<String,String> data;
     public ScheduleHandler() {
-        
+        csvHandler = new CSVHandler();
+        data = csvHandler.getData();
+    }
+    public int addEvent(String eventName, String date) {
+        String[] dateSplit = date.split(".");
+        int index = 0;
+        for(String word : dateSplit) {
+            char character = word.charAt(index);
+            if(!Character.isDigit(character)) {
+                System.err.println("Error in adding event " + dateSplit[0]);
+                return -1;
+            } else {
+                data.put(dateSplit[0],dateSplit[1]);
+            } 
+        }
+    return 1;
+    }
+    public int removeEvent(String name) {
+        data.remove(name);
+        return 1;
+    }
+    public int changeEvent(String typeOfchange, String eventName) {
+        if(typeOfchange.toLowerCase().equals("date change")) {
+            
+        }
+        return 1;
     }
 }
